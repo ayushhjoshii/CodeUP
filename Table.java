9665efa0ed2fa6cdbd8ac3ddc7ec3d93be76e3c7
Ayush Joshi <josayu127@gmail.com>
2025-09-16 09:12:56 +0530

    improved the functionality for some of the edge cases

--- Table.java
+++ Table.java
@@ -1,11 +1,33 @@
+//This program takes an input from the user and prints the table
+//Owner: Ayush Joshi
+
 import java.util.Scanner;
 public class Table {
     public static void main(String[] args){
+        printTable();
+    }
+
+    public static void printTable(){
         try(Scanner sc= new Scanner(System.in)){
-            int n= sc.nextInt();
-            for(int i=1; i<=10; i++){
-                System.out.println(n+" * "+i+" = "+(n*i));
+            int n, multiplier;
+            System.out.println("Number: ");
+            if(sc.hasNextInt()){
+                n=sc.nextInt();
+                System.out.println("Multiplier: ");
+                if(sc.hasNextInt()){
+                    multiplier= sc.nextInt();
+                    for(int i=1; i<=multiplier; i++){
+                        System.out.println(n+" * "+i+" = "+(n*i));
+                    }
+                }else{
+                    System.out.println("Error!! Please enter integer values only: ");
+                    printTable();
+                }
+            }else{
+                System.out.println("Error!! Please enter integer values only: ");
+                printTable();
             }
+            
         }
     }
 }
